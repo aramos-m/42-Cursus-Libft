30779611c2614e2bf84efe56406558d0f28cefea
Ana Ramos Moya <aramos-m@c3r11s1.42madrid.com>
2023-10-17 20:25:58 +0200

    Libft Part 1

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: aramos-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 20:57:17 by aramos-m          #+#    #+#             */
-/*   Updated: 2023/10/16 21:54:18 by aramos-m         ###   ########.fr       */
+/*   Updated: 2023/10/17 20:21:55 by aramos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,21 @@ char	*ft_strchr(const char *s, int c)
 	int	i;
 
 	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-			return ((char *) (s + i));
+	while (s[i] && s[i] != (unsigned char) c)
 		i++;
-	}
-	if (s[i] == c)
-		return ((char *) (s+i));
+	if (s[i] == (unsigned char) c)
+		return ((char *) s + i);
 	return (0);
 }
+/*#include <stdio.h>
+#include <string.h>
+
+int	main()
+{
+	char	str[10] = "Soy Ana";
+	int		c = 'I';
+
+	printf("Original function: %s\n", strchr(str, c));
+	printf("Created function: %s\n", ft_strchr(str, c));
+	return (0);
+}*/
--- libft/libft.h
+++ libft/libft.h
@@ -6,16 +6,18 @@
 /*   By: aramos-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 19:10:56 by aramos-m          #+#    #+#             */
-/*   Updated: 2023/10/16 21:22:31 by aramos-m         ###   ########.fr       */
+/*   Updated: 2023/10/17 20:24:20 by aramos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
 # include <stdlib.h>
 # include <unistd.h>
 
-int	ft_strlen(const char *s);
+int		ft_strlen(const char *s);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*ft_strchr(const char *s, int c);
 
 #endif
