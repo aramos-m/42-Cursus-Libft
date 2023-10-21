66db7e59adf670a0019077c683719b176fc8332f
aramos-m <anaana609@gmail.com>
2023-10-21 12:46:07 +0200

    Libft Part 1

---
+++ libft/ft_atoi.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramos-m <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 19:19:29 by aramos-m          #+#    #+#             */
+/*   Updated: 2023/10/21 12:40:09 by aramos-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	number;
+	int	sign;
+	int	i;
+
+	number = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == 32 || (str[i] > 8 && str[i] < 14))
+		i++;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		number = number * 10 + (str[i] - '0');
+		i++;
+	}
+	return (number * sign);
+}
+/*#include <stdio.h>
+#include <stdlib.h>
+int	main(void)
+{
+	char	str[] = "-1234a";
+
+	printf("Original function: %d\n", atoi(str));
+	printf("Created function: %d", ft_atoi(str));
+	return (0);
+}*/
