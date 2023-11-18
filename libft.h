5d604fa5c5f934a3777d493bb3ed11b872d39d5a
Ana Ramos Moya <144784967+aramos-m@users.noreply.github.com>
2023-11-18 19:15:54 +0100

    Libft Part 1

--- libft.h
+++ libft.h
@@ -25,7 +25,7 @@ int		ft_strlen(const char *s);
 void	*ft_memset(void *str, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t len);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 int		ft_toupper(int c);
