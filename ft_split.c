e1e87318d65608f48b810c6bc44423e5d0864f86
Ana Ramos Moya <aramos-m@c1r14s3.42madrid.com>
2023-11-19 20:16:48 +0100

    Liibft Part 2

--- ft_split.c
+++ ft_split.c
@@ -1,62 +1,117 @@
-//#include "libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramos-m <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/19 16:15:58 by aramos-m          #+#    #+#             */
+/*   Updated: 2023/11/19 18:27:55 by aramos-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-static char	*ft_divide(char const *s, char rule, int *k, int len)
+static char *ft_divide(char const *s, char rule, int *k, int len)
 {
-        int     i;
-        int     j;
-	char	*split;
+    int i = *k;
+    int j = 0;
+    char *split;
 
-	split = (char *)malloc(sizeof(char)*(len + 1));
-
-        i = *k;
-        j = 0;
-        while (s[i] != rule)
-        {
-                split[j] = s[i];
-                j++;
-                i++;
-        }
-	*k = i + 1;
-        split[j] = '\0';
-        return (split);
+    split = (char *)malloc(sizeof(char) * (len + 1));
+    if (split == NULL)
+        return (NULL);
+    while (s[i] != rule && s[i] != '\0')
+    {
+        split[j] = s[i];
+        j++;
+        i++;
+    }
+    *k = i;
+    split[j] = '\0';
+    return (split);
 }
 
-char	**ft_split(char const *s, char c)
+int	count_word(char const *s, char c)
 {
-        char    **result;
-        int     i;
-        int     *j;
-	int	len;
-	int	count = 1;
+	int	i;
+	int	word;
 
-        i = 0;
-	len = strlen(s);
-        j = malloc(sizeof(int));
-        result = malloc(sizeof(char *) * (len + 1));
-        if (result == NULL || j == NULL)
-                return (NULL);
-	*j = 0;
-        while (*j <= len)
+	word = 0;
+	i = 0;
+
+	while(s[i] && s[i] == c)
 	{
-                result[i] = ft_divide(s, c, j,len);
-		printf("%s\n",result[i]);
+		i++;
 	}
-	return (result);
+	while(s[i])
+	{
+		word ++;
+		while(s[i] && s[i] != c)
+			i++;
+		while(s[i] && s[i] == c)
+			i++;
+	}
+	return(word);
 }
 
-int main (void)
+char **ft_split(char const *s, char c)
 {
-        char const      s[] = "Hola, Ana, ¿cómo estás?";
-        char            **split;
-        char            rule = ' ';
-        int             k = 0;
+    char **result;
+    int i;
+    int j;
+    int word;
 
-	/*split = malloc(sizeof(char *)*( strlen(s) + 1));
-	split = ft_split(s,rule);
-	printf("%s\n",split[0]);*/
-	ft_split(s,rule);
-        return 0;
+	i = 0;
+	j = 0;
+	word = count_word(s, c);
+    result = (char **)malloc(sizeof(char *) * (word + 1));
+    if (result == NULL)
+        return (NULL);
+    while (s[j])
+    {
+	    if (s[j] != c)
+        {
+            result[i] = ft_divide(s, c, &j, word);
+            if (result[i] == NULL)
+            {
+                while (i > 0)
+                {
+                    i--;
+                    free(result[i]);
+                }
+                free(result);
+                return (NULL);
+            }
+            i++;
+        }
+        else
+            j++;
+    }
+    result[i] = NULL; // Marcar el final del array de punteros
+    return (result);
 }
+/*
+int main(void)
+{
+    char const s[] = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.";
+    char **split;
+    char rule = 'i'; 
+
+	printf("------\n");
+    split = ft_split(s, rule);
+
+    // Imprimir las filas resultantes
+	int	i = 0;
+	while (split[i])
+    {
+        printf("Fila %d: %s\n", i + 1, split[i]);
+        free(split[i]); // Liberar memoria de cada fila
+		i++;
+    }
+    free(split); // Liberar memoria del array de punteros
+    return 0;
+}*/
